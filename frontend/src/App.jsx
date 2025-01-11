e7e862385c51850ad4cf62ce2eedd87cfdc3d177
Veditya <adityarao.vedant@gmail.com>
2025-01-11 21:07:16 +0530

    completed frontend layout -> next is react routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+//import Login from "./pages/login/login";
+import Home from "./pages/home/home";
+//import Signup from "./pages/signup/signup";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
-    </>
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Home />
+    </div>
   );
 }
 
---
+++ frontend/src/components/sidebar/LogutButton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LogutButton = () => {
+  return <div></div>;
+};
+
+export default LogutButton;
