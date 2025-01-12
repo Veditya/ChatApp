7d675f334c348db1d61ebedf0ceb788325aa970f
Veditya <adityarao.vedant@gmail.com>
2025-01-12 18:21:17 +0530

    ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -12,14 +13,21 @@ import { app, server } from "./socket/socket.js";
 dotenv.config();
 const PORT = process.env.PORT || 8000;
 
+const __dirname = path.resolve();
+
 //middlewares
 app.use(express.json()); //to parse incoming json request
 app.use(cookieParser()); //to parse incoming cookies
 
 app.use("/api/auth", authRoute);
 app.use("/api/messages", messageRoute);
-
 app.use("/api/users", usersRoute);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 //server listening
 server.listen(5000, () => {
